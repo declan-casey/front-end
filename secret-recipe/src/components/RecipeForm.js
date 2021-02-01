b05759df2244e12172a305475b87351b549168bd
declan-casey <dpcaseyboy99@gmail.com>
2021-02-01 13:44:46 -0500

    finished logic for sign in

--- secret-recipe/src/components/RecipeForm.js
+++ secret-recipe/src/components/RecipeForm.js
@@ -1,21 +1,35 @@
 import React from 'react'
 
 //recipe_id
-
 //recipe_name
 //author
 //category
 //time
 //img
 //recipe_private T or F
+// (/api/recipes) post/get
+//(/api/recipies/:${id}) put/delete
 
-//steps
+//step_id
+//step_text
+//step_order (null)
+
+
+//ingredient_name
+//ingredient_id
+
+//step_ingredients  yakes in step and ingredient id
+//ingredient_quantity
 
 function RecipeForm() {
 
     return (
         <div>
             <form>
+                <input />
+                <input />
+                <input />
+                <input />
                 <input />
             </form>
         </div>
